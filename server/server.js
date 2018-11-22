76f8de37d3cb9f8df1ed7980b37711f62bace92d
Antonio Roldán <roldanpa@coventry.ac.uk>
2018-11-22 22:24:43 +0000

    add start server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = 4000
+
+app.get('/ping', (_, res) => {
+    res.send('pong')
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
+
+
+module.exports = app
