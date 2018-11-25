24e2ec7e0509e671340a9b0afc1def9013c10c39
Antonio Roldán <roldanpa@coventry.ac.uk>
2018-11-25 14:02:53 +0000

    Register login and logout endpoints

--- server/server.js
+++ server/server.js
@@ -11,6 +11,20 @@ app.use(cors())
 
 const port = 4000
 
+app.post('/register', (req, res) => {
+    db.registerUser(req.body.email, req.body.password, (err, data) => {
+        if(err) return res.status(data)
+        return res.send(data)
+    })
+})
+
+app.post('/logout', (req, res) => { 
+    db.logoutUser(req.body.session, (error, data) => {
+        if(error) return res.status(404).send(data)
+        return res.send(data)
+    })
+})
+
 app.get('/ping', (_, res) => {
     res.send('pong')
 })
